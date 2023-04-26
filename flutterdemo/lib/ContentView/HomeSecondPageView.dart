00ac20810e5b0867b2e1061cbf6b7e29ac72bcd2
HeShaoZe1 <heshaoze@gzchate.com>
2023-04-26 08:12:07 +0800

    添加边框修饰
    添加边框修饰

--- flutterdemo/lib/ContentView/HomeSecondPageView.dart
+++ flutterdemo/lib/ContentView/HomeSecondPageView.dart
@@ -5,12 +5,58 @@ class HomeSecondPageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
+      child: Container(
+        //color: Colors.green,
+        width: 50,
+        height: 50,
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(left: 10),
+        margin: EdgeInsets.only(right: 100),
+        child: Row(
+          children: [
+            Container(
+              child: Icon(
+                Icons.offline_bolt,
+                color: Colors.white,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                // border: Border(
+                //   left: BorderSide(
+                //       color: Colors.red, width: 3.0, style: BorderStyle.solid),
+                //   right: BorderSide(
+                //       color: Colors.green,
+                //       width: 4.0,
+                //       style: BorderStyle.solid),
+                // ),
+                border: Border.all(
+                    color: Colors.green, width: 4.0, style: BorderStyle.solid),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(5),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RichTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
       padding: EdgeInsets.all(30),
       child: RichText(
           text: TextSpan(
         text: "Baidu",
         style: TextStyle(
-            fontSize: 18, color: Colors.red, fontStyle: FontStyle.italic),
+            fontSize: 18,
+            color: Colors.red,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w400),
         children: [
           TextSpan(
             text: ".com",
